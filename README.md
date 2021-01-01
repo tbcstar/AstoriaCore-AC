0b667508e42350780e6fd10d31e8dcc0293efde5
RealLushen <53633753+RealLushen@users.noreply.github.com>
2021-01-01 21:48:02 +0100

    Release

---
+++ README.md
@@ -0,0 +1,85 @@
+<br />
+<p align="center">
+  <a href="http://playastoria.cc/">
+    <img src="https://i.ibb.co/rbsX12q/Unbenannt-1222.png" alt="AstoriaCore">
+  </a>
+
+  <h3 align="center">AstoriaCore 3.3.5a</h3>
+
+  <p align="center">
+    Community driven Classless MMO Framework. Proudly founded by Lushen
+and based on AzerothCore and TrinityCore :smile:
+    <br />
+    <a href="https://www.azerothcore.org/wiki/home"><strong>Explore the docs (AC) »</strong></a>
+    <br />
+    <br />
+    ·
+    <a href="#">Report Bug</a>
+    ·
+  </p>
+</p>
+
+## About AstoriaCore
+
+AstoriaCore is a customized Version of AzerothCore and the private source of WaloriaCore by Fractional aka Fred. With this project 
+we want to bring **Classless** to a new level. With many extensions, *AstoriaCore* is different from all other AzerothCore branches.
+*AstoriaCore* is the basis of the Classless Project "Project Astoria" which was managed by Lushen, Acidy and OMGhixD.
+
+### Built With
+
+AstoriaCore (and its Scripts/Modules) is built with
+
+* C++
+* C
+* LUA
+
+## Getting Started
+
+You want to create your own Classless Server? No problem, just get AstoriaCore! Here you will learn how:
+
+### Requirements:
+
+* [Visual Studio 2019 Community](https://visualstudio.microsoft.com/de/vs/)
+* [CMake 2.8](https://cmake.org/download/) or newer
+* [GIT Extension](https://git-scm.com/downloads)
+* [MySQL Server Community Edition](https://dev.mysql.com/downloads/mysql/5.7.html) (8.0 or 5.7)
+* [OpenSSL 1.1.x](https://slproweb.com/products/Win32OpenSSL.html) (Not the Light version!)
+* [Boost 1.72.0](https://www.boost.org/)
+
+For OpenSSL install the Version you want to use depending on your System Core (x64 or x86)
+
+### Clone the Repository
+
+This is an example of how to list things you need to use the software and how to install them.
+* npm
+  ```sh
+  git clone https://github.com/AstoriaCore/AstoriaCore-AC.git
+  ```
+
+### Installation
+
+Follow the [AzerothCore](https://www.azerothcore.org/wiki/Installation) Installation Documentation, to learn how to Compile and Build the Source.
+
+
+## License
+
+Distributed under the GNU AGPL 3.0 and GNU GPL 2.0 Licenses. See `LICENSE` for more information.
+
+
+
+## Contact
+
+Discord - `Lushen#4800`
+
+## Disclaimer
+
+We want to provide you with maximum security, yet we did not write the base of the code ourselves but used it from other OpenSource projects like AzerothCore 
+and TrinityCore. Therefore we do not guarantee security, correctness, usability of the code and all related programs, tools or Software you use. Also we do not 
+guarantee direct support for our code. All code written by us you use on your own responsibility. We also reserve the right to exclude people from the community 
+at any time who do not follow our guidelines or misuse code.
+
+## Credits
+
+* [AzerothCore Team](https://www.azerothcore.org/)
+* [TrinityCore Team](https://www.trinitycore.org/)
+* [AstoriaCore Team](https://discord.gg/wvDYdqX)
---
+++ src/tools/mmaps_generator/MapBuilder.h
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: http://github.com/azerothcore/azerothcore-wotlk/LICENSE-GPL2
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ */
+
+#ifndef _MAP_BUILDER_H
+#define _MAP_BUILDER_H
+
+#include <list>
+#include <vector>
+#include <set>
+#include <atomic>
+#include <map>
+#include <thread>
+
+#include "TerrainBuilder.h"
+#include "IntermediateValues.h"
+
+#include "Recast.h"
+#include "DetourNavMesh.h"
+#include "PCQueue.h"
+
+using namespace VMAP;
+
+namespace MMAP
+{
+    struct MapTiles
+    {
+        MapTiles() : m_mapId(uint32(-1)), m_tiles(nullptr) {}
+
+        MapTiles(uint32 id, std::set<uint32>* tiles) : m_mapId(id), m_tiles(tiles) {}
+        ~MapTiles() {}
+
+        uint32 m_mapId;
+        std::set<uint32>* m_tiles;
+
+        bool operator==(uint32 id)
+        {
+            return m_mapId == id;
+        }
+    };
+
+    typedef std::list<MapTiles> TileList;
+
+    struct Tile
+    {
+        Tile() : chf(nullptr), solid(nullptr), cset(nullptr), pmesh(nullptr), dmesh(nullptr) {}
+        ~Tile()
+        {
+            rcFreeCompactHeightfield(chf);
+            rcFreeContourSet(cset);
+            rcFreeHeightField(solid);
+            rcFreePolyMesh(pmesh);
+            rcFreePolyMeshDetail(dmesh);
+        }
+        rcCompactHeightfield* chf;
+        rcHeightfield* solid;
+        rcContourSet* cset;
+        rcPolyMesh* pmesh;
+        rcPolyMeshDetail* dmesh;
+    };
+
+    class MapBuilder
+    {
+    public:
+        MapBuilder(float maxWalkableAngle   = 70.f,
+                   bool skipLiquid          = false,
+                   bool skipContinents      = false,
+                   bool skipJunkMaps        = true,
+                   bool skipBattlegrounds   = false,
+                   bool debugOutput         = false,
+                   bool bigBaseUnit         = false,
+                   const char* offMeshFilePath = nullptr);
+
+        ~MapBuilder();
+
+        // builds all mmap tiles for the specified map id (ignores skip settings)
+        void buildMap(uint32 mapID);
+        void buildMeshFromFile(char* name);
+
+        // builds an mmap tile for the specified map and its mesh
+        void buildSingleTile(uint32 mapID, uint32 tileX, uint32 tileY);
+
+        // builds list of maps, then builds all of mmap tiles (based on the skip settings)
+        void buildAllMaps(unsigned int threads);
+
+        void WorkerThread();
+
+    private:
+        // detect maps and tiles
+        void discoverTiles();
+        std::set<uint32>* getTileList(uint32 mapID);
+
+        void buildNavMesh(uint32 mapID, dtNavMesh*& navMesh);
+
+        void buildTile(uint32 mapID, uint32 tileX, uint32 tileY, dtNavMesh* navMesh);
+
+        // move map building
+        void buildMoveMapTile(uint32 mapID,
+                              uint32 tileX,
+                              uint32 tileY,
+                              MeshData& meshData,
+                              float bmin[3],
+                              float bmax[3],
+                              dtNavMesh* navMesh);
+
+        void getTileBounds(uint32 tileX, uint32 tileY,
+                           float* verts, int vertCount,
+                           float* bmin, float* bmax);
+        void getGridBounds(uint32 mapID, uint32& minX, uint32& minY, uint32& maxX, uint32& maxY) const;
+
+        bool shouldSkipMap(uint32 mapID);
+        bool isTransportMap(uint32 mapID);
+        bool shouldSkipTile(uint32 mapID, uint32 tileX, uint32 tileY);
+        // percentageDone - method to calculate percentage
+        uint32 percentageDone(uint32 totalTiles, uint32 totalTilesDone);
+
+        TerrainBuilder* m_terrainBuilder;
+        TileList m_tiles;
+
+        bool m_debugOutput;
+
+        const char* m_offMeshFilePath;
+        bool m_skipContinents;
+        bool m_skipJunkMaps;
+        bool m_skipBattlegrounds;
+
+        float m_maxWalkableAngle;
+        bool m_bigBaseUnit;
+        // percentageDone - variables to calculate percentage
+        std::atomic<uint32> m_totalTiles;
+        std::atomic<uint32> m_totalTilesBuilt;
+
+        // build performance - not really used for now
+        rcContext* m_rcContext;
+
+        std::vector<std::thread> _workerThreads;
+        ProducerConsumerQueue<uint32> _queue;
+        std::atomic<bool> _cancelationToken;
+    };
+}
+
+#endif
